ef3331dcd1fd63dac8a88833793f036d7db812e6
Nick Boughton <nicholasboughton@googlemail.com>
2016-01-27 18:22:42 +0000

    added PyTriplets

--- check/check.go
+++ check/check.go
@@ -14,6 +14,14 @@ func Prime(n int64) bool {
 	return big.NewInt(n).ProbablyPrime(10)
 }
 
+// PyTriplet returns true if a < b < c and a^2 + b^2 = c^2
+func PyTriplet(a, b, c int64) bool {
+	if a < b && b < c && a*a+b*b == c*c {
+		return true
+	}
+	return false
+}
+
 // UniqueCharString returns true if a string contains no duplicate
 // characters
 func UniqueCharString(b []byte) bool {
