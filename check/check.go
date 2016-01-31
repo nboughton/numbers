d2141d5d352f7c2d65a73c27cfdb29a2885e5c25
Nick Boughton <nicholasboughton@googlemail.com>
2016-01-31 15:25:39 +0000

    Added slice functions pandigital check

--- check/check.go
+++ check/check.go
@@ -5,13 +5,36 @@ such as primes, palindromes etc...
 package check
 
 import (
+	"github.com/nboughton/numbers/slice"
 	"math/big"
 	"reflect"
+	"sort"
 )
 
 // Prime returns true if n is prime
 func Prime(n int64) bool {
 	return big.NewInt(n).ProbablyPrime(10)
+}
+
+var panDigitalSets = map[int][]int64{
+	1: {1},
+	2: {1, 2},
+	3: {1, 2, 3},
+	4: {1, 2, 3, 4},
+	5: {1, 2, 3, 4, 5},
+	6: {1, 2, 3, 4, 5, 6},
+	7: {1, 2, 3, 4, 5, 6, 7},
+	8: {1, 2, 3, 4, 5, 6, 7, 8},
+	9: {1, 2, 3, 4, 5, 6, 7, 8, 9},
+}
+
+// Pandigital returns true if n contains 1-n digits with each digit
+// appearing only once
+func Pandigital(n int) bool {
+	t := slice.IntToSlice(n)
+	sort.Ints(t)
+
+	return reflect.DeepEqual(panDigitalSets[len(t)], t)
 }
 
 // PyTriplet returns true if a < b < c and a^2 + b^2 = c^2
--- slice/slice.go
+++ slice/slice.go
@@ -8,26 +8,51 @@ import (
 	"strings"
 )
 
-// SliceToInt takes a slice of ints and returns their composite integer
-func SliceToInt(n []int64) int64 {
+// SliceToInt64 takes a slice of int64s and returns their composite int64
+func SliceToInt64(n []int64) int64 {
 	s := ""
 
 	for _, v := range n {
-		s = s + strconv.FormatInt(v, 10)
+		s += strconv.FormatInt(v, 10)
 	}
 
 	i, _ := strconv.Atoi(s)
 	return int64(i)
 }
 
-// IntToSlice returns a number as a slice of its digits
-func IntToSlice(n int64) []int64 {
+// SliceToInt64 takes a slice of ints and returns their composite int
+func SliceToInt(n []int) int {
+	s := ""
+
+	for _, v := range n {
+		s += strconv.Itoa(v)
+	}
+
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+// Int64ToSlice returns a number as a slice of its digits
+func Int64ToSlice(n int64) []int64 {
 	nStrs := strings.Split(strconv.FormatInt(n, 10), "")
 	nInts := []int64{}
 
 	for _, v := range nStrs {
 		i, _ := strconv.Atoi(v)
 		nInts = append(nInts, int64(i))
+	}
+
+	return nInts
+}
+
+// IntToSlice returns a number as a slice of its digits
+func IntToSlice(n int) []int {
+	nStrs := strings.Split(strconv.Itoa(n), "")
+	nInts := []int{}
+
+	for _, v := range nStrs {
+		i, _ := strconv.Atoi(v)
+		nInts = append(nInts, i)
 	}
 
 	return nInts
