77a4d5f1217c21836d27f8a33e711e0440872d03
n.boughton <nicholasboughton@googlemail.com>
2016-02-02 14:10:05 +0000

    still can't see why this isn't getting the right numbers for p47

--- factor/factor.go
+++ factor/factor.go
@@ -4,6 +4,7 @@ Package factor - provides functions for return factors, divisors and factorials
 package factor
 
 import (
+	//"fmt"
 	"github.com/cznic/sortutil"
 	"github.com/nboughton/numbers/check"
 	"github.com/nboughton/numbers/total"
@@ -14,26 +15,24 @@ import (
 
 // Primes returns int64 slice of prime factors of n
 func Primes(n int64) []int64 {
-	p := []int64{}
+	p1 := []int64{}
 	t := int64(math.Sqrt(float64(n)))
 
 	if n%2 == 0 {
-		p = append(p, int64(2))
+		p1 = append(p1, int64(2))
 	}
 
-	if n < 100 {
+	if n < 1000 {
 		t = n/2 + 1
 	}
 
-	for i := int64(3); i < t; i += 2 {
+	for i := int64(3); i <= t; i += 2 {
 		if n%i == 0 && check.Prime(i) {
-			p = append(p, i)
-			if i*i != n && check.Prime(i*i) {
-				p = append(p, i*i)
-			}
+			p1 = append(p1, i)
 		}
 	}
-	return p
+
+	return p1
 }
 
 // Divisors returns int64 slice of divisors of n
