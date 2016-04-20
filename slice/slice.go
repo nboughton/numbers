fd0281819a0c59c0bd68faa32eb759dae6920010
Nick Boughton <nicholasboughton@googlemail.com>
2016-04-20 19:29:20 +0100

    code cleanup to make better use of strconv functions

--- slice/slice.go
+++ slice/slice.go
@@ -10,47 +10,47 @@ import (
 
 // ToBigInt returns a big Int composite of the digits in n
 func ToBigInt(n []int64) *big.Int {
-	s := ""
+	s := []byte{}
 
 	for _, v := range n {
-		s += strconv.FormatInt(v, 10)
+		s = strconv.AppendInt(s, v, 10)
 	}
 
-	i, _ := big.NewInt(0).SetString(s, 10)
+	i, _ := big.NewInt(0).SetString(string(s), 10)
 	return i
 }
 
 // ToInt64 takes a slice of int64s and returns their composite int64
 func ToInt64(n []int64) int64 {
-	s := ""
+	s := []byte{}
 
 	for _, v := range n {
-		s += strconv.FormatInt(v, 10)
+		s = strconv.AppendInt(s, v, 10)
 	}
 
-	i, _ := strconv.Atoi(s)
-	return int64(i)
+	i, _ := strconv.ParseInt(string(s), 10, 64)
+	return i
 }
 
 // ToInt takes a slice of ints and returns their composite int
 func ToInt(n []int) int {
-	s := ""
+	s := []byte{}
 
 	for _, v := range n {
-		s += strconv.Itoa(v)
+		s = strconv.AppendInt(s, int64(v), 10)
 	}
 
-	i, _ := strconv.Atoi(s)
-	return i
+	i, _ := strconv.ParseInt(string(s), 10, 32)
+	return int(i)
 }
 
 // FromInt64 returns a number as a slice of its digits
 func FromInt64(n int64) []int64 {
-	nStrs, nInts := []byte((strconv.FormatInt(n, 10))), []int64{}
+	nInts := []int64{}
 
-	for _, v := range nStrs {
-		i, _ := strconv.Atoi(string(v))
-		nInts = append(nInts, int64(i))
+	for _, v := range []byte((strconv.FormatInt(n, 10))) {
+		i, _ := strconv.ParseInt(string(v), 10, 64)
+		nInts = append(nInts, i)
 	}
 
 	return nInts
@@ -58,9 +58,9 @@ func FromInt64(n int64) []int64 {
 
 // FromInt returns a number as a slice of its digits
 func FromInt(n int) []int {
-	nStrs, nInts := []byte((strconv.Itoa(n))), []int{}
+	nInts := []int{}
 
-	for _, v := range nStrs {
+	for _, v := range []byte((strconv.Itoa(n))) {
 		i, _ := strconv.Atoi(string(v))
 		nInts = append(nInts, i)
 	}
