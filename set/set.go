061ffb7afa520c3169422cc865af1337e8396805
Nick Boughton <nicholasboughton@gmail.com>
2018-06-22 16:18:56 +0100

    have find stream results as a channel

--- set/set.go
+++ set/set.go
@@ -37,14 +37,18 @@ func (s Int64s) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s Int64s) Less(i, j int) bool { return len(s[i]) < len(s[j]) }
 
 // Find returns all sets in s that contain n
-func (s Int64s) Find(n int64) Int64s {
-	var res Int64s
+func (s Int64s) Find(n int64) chan Int64 {
+	c := make(chan Int64)
 
-	for _, set := range s {
-		if set.Contains(n) {
-			res = append(res, set)
+	go func() {
+		defer close(c)
+
+		for _, set := range s {
+			if set.Contains(n) {
+				c <- set
+			}
 		}
-	}
+	}()
 
-	return res
+	return c
 }
